10e278395701fe58793de722f236b956ad56942f
Giacomo Rosin <giacomorosin@gmail.com>
2020-12-24 18:11:23 +0100

    Implement SelectAppModeActivity
    Update strings.xml and add dimens.xml

---
+++ app/src/main/java/it/unive/quadcore/smartmeal/ui/SelectAppModeActivity.java
@@ -0,0 +1,64 @@
+package it.unive.quadcore.smartmeal.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import it.unive.quadcore.smartmeal.R;
+import it.unive.quadcore.smartmeal.storage.ApplicationMode;
+import it.unive.quadcore.smartmeal.storage.Storage;
+import it.unive.quadcore.smartmeal.ui.customer.CustomerHomeActivity;
+import it.unive.quadcore.smartmeal.ui.customer.InsertPersonalDataActivity;
+
+public class SelectAppModeActivity extends AppCompatActivity {
+
+    private Button customerButton;
+    private Button managerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Storage.initializeStorage(this);
+
+        // TODO attenzione rimuovere per versione finale
+        // commentare / decommentare per poter cambiare modalità app
+        Storage.setApplicationMode(ApplicationMode.UNDEFINED);
+
+        switch (Storage.getApplicationMode()) {
+            case UNDEFINED:
+                setContentView(R.layout.activity_select_app_mode);
+                break;
+            case CUSTOMER:
+                startActivity(new Intent(this, CustomerHomeActivity.class));
+                finish();
+                return;
+            case MANAGER:
+                // TODO rimpiazzare ManagerHomeActivity con il nome dell'Activity del Manager
+                // e decommentare
+//                startActivity(new Intent(this, ManagerHomeActivity.class));
+//                finish();
+                return;
+            default:
+                throw new IllegalStateException("Unexpected: Storage.getApplicationMode() returned null");
+        }
+
+        customerButton = findViewById(R.id.customer_button);
+        managerButton = findViewById(R.id.manager_button);
+
+        customerButton.setOnClickListener(v -> {
+            // avvia l'activity che richiede i dati dell'utente
+            startActivity(new Intent(SelectAppModeActivity.this, InsertPersonalDataActivity.class));
+        });
+
+        managerButton.setOnClickListener(v -> {
+            // avvia l'activity che richiede la password per accedere alla modalità MANAGER
+            // TODO rimpiazzare InsertPasswordActivity con il nome dell'Activity corretto
+
+//            startActivity(new Intent(SelectAppModeActivity.this, InsertPasswordActivity.class));
+        });
+
+    }
+}
