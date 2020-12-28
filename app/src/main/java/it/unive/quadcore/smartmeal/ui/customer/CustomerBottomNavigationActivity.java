0c00bc11634225d518e8b85d27924ad1fef76528
Giacomo Rosin <giacomorosin@gmail.com>
2020-12-29 00:38:02 +0100

    Add FloatingActionButton to CustomerBottomNavigationActivity
    It launches the customer virtual room (CustomerVirtualRoomActivity)

--- app/src/main/java/it/unive/quadcore/smartmeal/ui/customer/CustomerBottomNavigationActivity.java
+++ app/src/main/java/it/unive/quadcore/smartmeal/ui/customer/CustomerBottomNavigationActivity.java
@@ -1,8 +1,11 @@
 package it.unive.quadcore.smartmeal.ui.customer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -14,11 +17,13 @@ import it.unive.quadcore.smartmeal.R;
 
 public class CustomerBottomNavigationActivity extends AppCompatActivity {
 
+    private FloatingActionButton startCustomerVirtualRoomFab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_bottom_navigation);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = findViewById(R.id.customer_nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -27,6 +32,17 @@ public class CustomerBottomNavigationActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        startCustomerVirtualRoomFab = findViewById(R.id.start_customer_virtual_room_fab);
+        startCustomerVirtualRoomFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(
+                        CustomerBottomNavigationActivity.this,
+                        CustomerVirtualRoomActivity.class
+                ));
+            }
+        });
     }
 
 }
