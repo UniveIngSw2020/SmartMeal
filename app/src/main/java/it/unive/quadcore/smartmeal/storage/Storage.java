77e73cde07475ae7d7f1813bfea83b7f3d51cea2
Giacomo Rosin <giacomorosin@gmail.com>
2020-12-28 01:02:06 +0100

    Update Storage and SelectAppModeActivity
    Storage now has a method to check whether it's initialized (`isInitialized`)
    
    SelectAppModeActivity now uses this method.

--- app/src/main/java/it/unive/quadcore/smartmeal/storage/Storage.java
+++ app/src/main/java/it/unive/quadcore/smartmeal/storage/Storage.java
@@ -139,64 +139,76 @@ public class Storage {
         localDescription = new LocalDescription("The boat restourant",presentation, location/*, imageID*/ ,menu);
 
         return localDescription;
-
     }
+
+
+    /**
+     * Permette di verificare se Storage è gia stata inizializzata
+     * (tramite il metodo `initializeStorage(Activity)`)
+     * e di conseguenza se è pronta all'uso.
+     *
+     * @return true se è già stata inizializzata, false altrimenti
+     */
+    public static boolean isInitialized() {
+        return initialized;
+    }
+
 
     // Generazione dei prodotti
     private static Set<Product> generateProducts(){
         Set<Product> products = new TreeSet<>();
 
-        Product product = new Product("Tomato soup",new Money(200), FoodCategory.STARTERS,"");
+        Product product = new Product("Tomato soup",new Money(200), FoodCategory.STARTERS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("French onion soup",new Money(250), FoodCategory.STARTERS,"");
+        product = new Product("French onion soup",new Money(250), FoodCategory.STARTERS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Tomato salad",new Money(290), FoodCategory.SALADS,"");
+        product = new Product("Tomato salad",new Money(290), FoodCategory.SALADS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Chicken salad",new Money(330), FoodCategory.SALADS,"");
+        product = new Product("Chicken salad",new Money(330), FoodCategory.SALADS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("German sausage and chips",new Money(650), FoodCategory.MAIN_COURSES,"");
+        product = new Product("German sausage and chips",new Money(650), FoodCategory.MAIN_COURSES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Grilled fish and potatoes",new Money(625), FoodCategory.MAIN_COURSES,"");
+        product = new Product("Grilled fish and potatoes",new Money(625), FoodCategory.MAIN_COURSES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Italian cheese and tomato pizza",new Money(480), FoodCategory.MAIN_COURSES,"");
+        product = new Product("Italian cheese and tomato pizza",new Money(480), FoodCategory.MAIN_COURSES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Thai chicken and rice",new Money(590), FoodCategory.MAIN_COURSES,"");
+        product = new Product("Thai chicken and rice",new Money(590), FoodCategory.MAIN_COURSES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Vegetable pasta",new Money(480), FoodCategory.MAIN_COURSES,"");
+        product = new Product("Vegetable pasta",new Money(480), FoodCategory.MAIN_COURSES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Roast chicken and potatoes",new Money(590), FoodCategory.MAIN_COURSES,"");
+        product = new Product("Roast chicken and potatoes",new Money(590), FoodCategory.MAIN_COURSES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Cheeseburger",new Money(320), FoodCategory.SIDE_DISHES,"");
+        product = new Product("Cheeseburger",new Money(320), FoodCategory.SIDE_DISHES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Vegetable omelette",new Money(325), FoodCategory.SIDE_DISHES,"");
+        product = new Product("Vegetable omelette",new Money(325), FoodCategory.SIDE_DISHES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Cheese and tomato sandwich",new Money(320), FoodCategory.SIDE_DISHES,"");
+        product = new Product("Cheese and tomato sandwich",new Money(320), FoodCategory.SIDE_DISHES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Burger",new Money(290), FoodCategory.SIDE_DISHES,"");
+        product = new Product("Burger",new Money(290), FoodCategory.SIDE_DISHES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Chicken sandwich",new Money(350), FoodCategory.SIDE_DISHES,"");
+        product = new Product("Chicken sandwich",new Money(350), FoodCategory.SIDE_DISHES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Cheese omelette",new Money(350), FoodCategory.SIDE_DISHES,"");
+        product = new Product("Cheese omelette",new Money(350), FoodCategory.SIDE_DISHES,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Mineral water",new Money(100), FoodCategory.DRINKS,"");
+        product = new Product("Mineral water",new Money(100), FoodCategory.DRINKS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Fresh orange juice",new Money(120), FoodCategory.DRINKS,"");
+        product = new Product("Fresh orange juice",new Money(120), FoodCategory.DRINKS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Soft drinks",new Money(130), FoodCategory.DRINKS,"");
+        product = new Product("Soft drinks",new Money(130), FoodCategory.DRINKS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("English tea",new Money(90), FoodCategory.DRINKS,"");
+        product = new Product("English tea",new Money(90), FoodCategory.DRINKS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Irish cream coffee",new Money(90), FoodCategory.DRINKS,"");
+        product = new Product("Irish cream coffee",new Money(90), FoodCategory.DRINKS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Fruit salad and cream",new Money(220), FoodCategory.DESSERTS,"");
+        product = new Product("Fruit salad and cream",new Money(220), FoodCategory.DESSERTS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Ice cream",new Money(200), FoodCategory.DESSERTS,"");
+        product = new Product("Ice cream",new Money(200), FoodCategory.DESSERTS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Lemon cake",new Money(220), FoodCategory.DESSERTS,"");
+        product = new Product("Lemon cake",new Money(220), FoodCategory.DESSERTS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Cheese and biscuit",new Money(220), FoodCategory.DESSERTS,"");
+        product = new Product("Cheese and biscuit",new Money(220), FoodCategory.DESSERTS,"Tonno, Asparagi, Cipolle");
         products.add(product);
-        product = new Product("Chocolate chake",new Money(250), FoodCategory.DESSERTS,"");
+        product = new Product("Chocolate chake",new Money(250), FoodCategory.DESSERTS,"Tonno, Asparagi, Cipolle");
         products.add(product);
 
         return products;
