5b9c8ebb94f13e7d4fcd15061be0d5dacf87fb89
Giacomo Rosin <giacomorosin@gmail.com>
2020-12-30 23:51:35 +0100

    Refactor catch into if + instanceof in TableAdapter TableException handling

--- app/src/main/java/it/unive/quadcore/smartmeal/ui/customer/virtualroom/TableAdapter.java
+++ app/src/main/java/it/unive/quadcore/smartmeal/ui/customer/virtualroom/TableAdapter.java
@@ -92,41 +92,42 @@ public class TableAdapter extends RecyclerView.Adapter<TableAdapter.TableViewHol
                                     .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                                     .replace(R.id.customer_room_fragment_container, customerVirtualRoomFragment)
                                     .commit();
-
-                        } catch(AlreadyOccupiedTableException e) {
-                            Log.i(TAG, "AlreadyOccupiedTableException happened: " + e.getMessage());
-                            Snackbar.make(
-                                    v.findViewById(android.R.id.content),
-                                    R.string.already_occupied_table_snackbar,
-                                    BaseTransientBottomBar.LENGTH_LONG
-                            ).show();
-                        } catch(NoSuchTableException e) {
-                            Log.i(TAG, "NoSuchTableException happened: " + e.getMessage());
-                            Snackbar.make(
-                                    v.findViewById(android.R.id.content),
-                                    R.string.unexpected_table_error_snackbar,
-                                    BaseTransientBottomBar.LENGTH_LONG
-                            ).show();
-                        } catch(AlreadyAssignedTableException e) { //TODO: needs to have priority in Local over AlreadyOccupiedException
-                            Log.i(TAG, "AlreadyAssignedTableException happened: " + e.getMessage());
                         } catch (TableException e) {
-                            Log.i(TAG, "TableException happened: " + e.getMessage());
-                            Snackbar.make(
-                                    v.findViewById(android.R.id.content),
-                                    R.string.unexpected_table_error_snackbar,
-                                    BaseTransientBottomBar.LENGTH_LONG
-                            ).show();
 
-                            // 1. Tavolo già occupato
-                            // avvisare cliente, aggiornare lista tavoli e far scegliere un altro tavolo
+                            if (e instanceof AlreadyOccupiedTableException) {
+                                // 1. Tavolo già occupato
+                                // avvisare cliente, aggiornare lista tavoli e far scegliere un altro tavolo
+                                Log.i(TAG, "AlreadyOccupiedTableException happened: " + e.getMessage());
+                                Snackbar.make(
+                                        v.findViewById(android.R.id.content),
+                                        R.string.already_occupied_table_snackbar,
+                                        BaseTransientBottomBar.LENGTH_LONG
+                                ).show();
+                            } else if (e instanceof NoSuchTableException) {
+                                // 2. Tavolo non esiste
+                                // avvisare il cliente con un errore generico e farlo ritentare
+                                Log.i(TAG, "NoSuchTableException happened: " + e.getMessage());
+                                Snackbar.make(
+                                        v.findViewById(android.R.id.content),
+                                        R.string.unexpected_table_error_snackbar,
+                                        BaseTransientBottomBar.LENGTH_LONG
+                                ).show();
+                            } else if (e instanceof AlreadyAssignedTableException) {
+                                // TODO: needs to have priority in Local over AlreadyOccupiedException
 
-                            // 2. Tavolo non esiste
-                            // avvisare il cliente con un errore generico e farlo ritentare
+                                // 3. Il cliente ha già il tavolo che ha richiesto
+                                // 4. Il cliente ha già un tavolo (diverso da quello richiesto)
 
-                            // 3. Il cliente ha già il tavolo che ha richiesto
-
-                            // 4. Il cliente ha già un tavolo (diverso da quello richiesto)
-
+                                Log.i(TAG, "AlreadyAssignedTableException happened: " + e.getMessage());
+                            } else {
+                                // errore inaspettato
+                                Log.i(TAG, "TableException happened: " + e.getMessage());
+                                Snackbar.make(
+                                        v.findViewById(android.R.id.content),
+                                        R.string.unexpected_table_error_snackbar,
+                                        BaseTransientBottomBar.LENGTH_LONG
+                                ).show();
+                            }
                         }
                     }
                 });
