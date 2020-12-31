b8ed66e9b10968ea3791374542d62d8258aa657b
EnricoPittini <48480569+EnricoPittini@users.noreply.github.com>
2020-12-31 16:10:33 +0100

    Add Table Activity

---
+++ app/src/main/java/it/unive/quadcore/smartmeal/ui/manager/bottomnavigation/AddTableActivity.java
@@ -0,0 +1,16 @@
+package it.unive.quadcore.smartmeal.ui.manager.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.unive.quadcore.smartmeal.R;
+
+public class AddTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_table);
+    }
+}
