56aa5507ba633f3e61a7c74ea49f1ca5b39347d6
Giacomo Rosin <giacomorosin@gmail.com>
2021-01-02 22:50:48 +0100

    Add permission request for Nearby

--- app/src/main/java/it/unive/quadcore/smartmeal/ui/SelectAppModeActivity.java
+++ app/src/main/java/it/unive/quadcore/smartmeal/ui/SelectAppModeActivity.java
@@ -1,8 +1,13 @@
 package it.unive.quadcore.smartmeal.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -16,6 +21,18 @@ import it.unive.quadcore.smartmeal.ui.customer.InsertPersonalDataActivity;
 public class SelectAppModeActivity extends AppCompatActivity {
 
     private static final String TAG = "SelectAppModeActivity";
+
+    private static final String[] REQUIRED_PERMISSIONS = new String[] {
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
+
     private Button customerButton;
     private Button managerButton;
 
@@ -66,5 +83,48 @@ public class SelectAppModeActivity extends AppCompatActivity {
 //            startActivity(new Intent(SelectAppModeActivity.this, InsertPasswordActivity.class));
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // TODO testare, in teoria le altre versioni hanno permessi in automatico
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!hasPermission(this, REQUIRED_PERMISSIONS)) {
+                requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+            }
+        }
+    }
+
+
+// TODO la tengo in caso serva in futuro
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+//            return;
+//        }
+//
+//        for (int grantResult : grantResults) {
+//            if (grantResult == PackageManager.PERMISSION_DENIED) {
+//                Snackbar.make(
+//                        findViewById(android.R.id.content),
+//                        "Required",
+//                        BaseTransientBottomBar.LENGTH_LONG
+//                ).show();
+//            }
+//        }
+//    }
+
+    private static boolean hasPermission(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
