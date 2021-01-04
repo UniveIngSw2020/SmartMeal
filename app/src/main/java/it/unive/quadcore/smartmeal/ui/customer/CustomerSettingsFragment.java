5e821e7eeb1d5ccd364bdb0dc40a783ad1ae1392
Giacomo Rosin <giacomorosin@gmail.com>
2021-01-04 10:39:59 +0100

    Remove old imports (fix compilation error)

--- app/src/main/java/it/unive/quadcore/smartmeal/ui/customer/CustomerSettingsFragment.java
+++ app/src/main/java/it/unive/quadcore/smartmeal/ui/customer/CustomerSettingsFragment.java
@@ -5,8 +5,6 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
