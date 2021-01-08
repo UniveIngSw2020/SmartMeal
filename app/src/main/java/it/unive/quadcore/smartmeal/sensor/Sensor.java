83fe1c205346915cc751ef6982274ef255dd429a
EnricoPittini <48480569+EnricoPittini@users.noreply.github.com>
2021-01-08 18:53:17 +0100

    Sensor

--- app/src/main/java/it/unive/quadcore/smartmeal/sensor/Sensor.java
+++ app/src/main/java/it/unive/quadcore/smartmeal/sensor/Sensor.java
@@ -1,6 +1,11 @@
 package it.unive.quadcore.smartmeal.sensor;
 
+import com.google.android.gms.location.GeofencingClient;
+
 public abstract class Sensor {
+
+    private GeofencingClient geofencingClient;
+
     private Runnable onShakeDetectedCallback;
     private Runnable onEntranceCallback;
 
