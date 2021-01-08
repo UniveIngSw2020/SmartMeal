84748dc7927cac21c1228779a5392fe8d4a3687b
EnricoPittini <48480569+EnricoPittini@users.noreply.github.com>
2021-01-08 19:23:08 +0100

    Sensor

---
+++ app/src/main/java/it/unive/quadcore/smartmeal/sensor/GeofenceBroadcastReceiver.java
@@ -0,0 +1,37 @@
+package it.unive.quadcore.smartmeal.sensor;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+
+    // Ricevuto evento geofence
+    public void onReceive(Context context, Intent intent) {
+        // Geofence event
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
+        if (geofencingEvent.hasError()) { // Errore evento geofence
+            // TODO : implementare
+            return;
+        }
+
+        // Geofence transition type
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Testo geofence transition type:  vedo se è quella che mi interessa. A me interessa solo entrata.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+
+            // CHIAMARE ONENTRANCECALLBACK
+            Sensor.onEntranceCallback.run();
+
+
+        } else { // Geofence transition type non è quella di mio interesse
+            // TODO : implementare
+        }
+    }
+}
--- app/src/main/java/it/unive/quadcore/smartmeal/sensor/Sensor.java
+++ app/src/main/java/it/unive/quadcore/smartmeal/sensor/Sensor.java
@@ -1,17 +1,133 @@
 package it.unive.quadcore.smartmeal.sensor;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unive.quadcore.smartmeal.storage.CustomerStorage;
 
 public abstract class Sensor {
 
     private GeofencingClient geofencingClient;
+    private List<Geofence> geofenceList;
 
     private Runnable onShakeDetectedCallback;
-    private Runnable onEntranceCallback;
+    static Runnable onEntranceCallback;
+
+    private PendingIntent geofencePendingIntent;
+
+
 
     public abstract void startShakeDetection(Runnable onShakeDetectedCallback);
+
     public abstract void endShakeDetection();
 
-    public abstract void startEntranceDetection(Runnable onEntranceCallback);
-    public abstract void endEntranceDetection();
+
+
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(Geofence.GEOFENCE_TRANSITION_ENTER);
+        builder.addGeofences(geofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent(Activity activity) {
+        // Reuse the PendingIntent if we already have it.
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(activity, GeofenceBroadcastReceiver.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        geofencePendingIntent = PendingIntent.getBroadcast(activity, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
+    }
+
+    public void startEntranceDetection(Runnable onEntranceCallback, Activity activity) {
+
+        Sensor.onEntranceCallback = onEntranceCallback;
+
+        geofencingClient = LocationServices.getGeofencingClient(activity);
+
+        geofenceList = new ArrayList<>();
+
+        Location location = CustomerStorage.getLocalDescription().getLocation();
+
+        float radius = 5;
+
+        geofenceList.add(new Geofence.Builder()
+                // Id geofence
+                .setRequestId("geofence")
+
+                .setCircularRegion(
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        radius
+                )
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build());
+
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent(activity))
+                .addOnSuccessListener(activity, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Geofences added
+                        // ...
+                    }
+                })
+                .addOnFailureListener(activity, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to add geofences // TODO : eccezione
+                        // ...
+                    }
+                });
+
+    }
+    public void endEntranceDetection(Activity activity){
+        geofencingClient.removeGeofences(getGeofencePendingIntent(activity))
+                .addOnSuccessListener(activity, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Geofences removed
+                        // ...
+                    }
+                })
+                .addOnFailureListener(activity, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to remove geofences
+                        // ...
+                    }
+                });
+    }
 }
